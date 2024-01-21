55ea184c0540fb2da37ddd523fae1dac26574e4c
saeed <saeedhosseny1377@gmail.com>
2024-01-21 21:35:39 +0330

    update

--- ios/Classes/CommnunicationNotificationPlugin.swift
+++ ios/Classes/CommnunicationNotificationPlugin.swift
@@ -137,10 +137,8 @@ class CommunicationNotificationPlugin {
     }
 
     func cancelNotification(_ identifier : String){
-        UNUserNotificationCenter.current().getPendingNotificationRequests { (notificationRequests) in
-   var identifiers: [String] = [identifier]
-   UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: identifiers)
+  UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [identifier])
    return
 }
-    }
+
 }
